5e13d3d736c2b74820e1538b695c72ef53a8cba7
cristianbyte <charlsmaritz@gmail.com>
2023-01-02 23:55:55 -0500

    horas de idiotismo para borrar un scroll imposible de hacer flotar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,22 +12,45 @@ export default function Navbar(){
     const activateContact = (id) => {
       setActiveContact(id);
     }
+
+    const root = document.documentElement;
+    
+    function disableScrolling() {
+        setTimeout(()=>{
+            root.style.setProperty('--foo', '#18f26488');
+            setTimeout(()=>{
+                root.style.setProperty('--foo', '#18f26444');
+                setTimeout(()=>{
+                    root.style.setProperty('--foo', '#18f26400');
+                }, 60);
+            }, 60);
+        }, 60);
+    }
+    
+    function enableScrolling() {
+        root.style.setProperty('--foo', '#18f264');
+
+    }
+
     
     return (
         <div className="navbar">
             <Search/>
             {/* map.contacts */}
-            <Contact onClick={() => activateContact(1)} active={activeContact === 1} name='Me' picture={image}   state='state__active' message='Hola ¿Como estás?, tenia tiempo sin saber de ti' notification='1'/>
-            <Contact onClick={() => activateContact(2)} active={activeContact === 2} name='Meow' picture={image} state='state__active' message='Hola' notification='1'/>
-            <Contact onClick={() => activateContact(3)} active={activeContact === 3} name='Less' picture={image} state='state__active' message='Hola' notification='1'/>
-            <Contact onClick={() => activateContact(4)} active={activeContact === 4} name='More' picture={image} state='state__active' message='Hola' notification='1'/>
-            <Contact onClick={() => activateContact(4)} active={activeContact === 4} name='More' picture={image} state='state__active' message='Hola' notification='1'/>
-            <Contact onClick={() => activateContact(4)} active={activeContact === 4} name='More' picture={image} state='state__active' message='Hola' notification='1'/>
-            <Contact onClick={() => activateContact(4)} active={activeContact === 4} name='More' picture={image} state='state__active' message='Hola' notification='1'/>
-            <Contact onClick={() => activateContact(4)} active={activeContact === 4} name='More' picture={image} state='state__active' message='Hola' notification='1'/>
-            <Contact onClick={() => activateContact(4)} active={activeContact === 4} name='More' picture={image} state='state__active' message='Hola' notification='1'/>
-            
-            <Contact onClick={() => activateContact(4)} active={activeContact === 4} name='More' picture={image} state='state__active' message='Hola' notification='1'/>
-        </div>
+            <div className="navbar__chats" onScroll={enableScrolling} 
+            onMouseLeave={disableScrolling}
+            onMouseEnter={enableScrolling} >
+                <Contact onClick={() => activateContact(1)} active={activeContact === 1} name='Me' picture={image}   state='state__active' message='Hola ¿Como estás?, tenia tiempo sin saber de ti' notification='1'/>
+                <Contact onClick={() => activateContact(2)} active={activeContact === 2} name='Meow' picture={image} state='state__active' message='Hola' notification='1'/>
+                <Contact onClick={() => activateContact(3)} active={activeContact === 3} name='Less' picture={image} state='state__active' message='Hola' notification='1'/>
+                <Contact onClick={() => activateContact(4)} active={activeContact === 4} name='More' picture={image} state='state__active' message='Hola' notification='1'/>
+                <Contact onClick={() => activateContact(4)} active={activeContact === 4} name='More' picture={image} state='state__active' message='Hola' notification='1'/>
+                <Contact onClick={() => activateContact(4)} active={activeContact === 4} name='More' picture={image} state='state__active' message='Hola' notification='1'/>
+                <Contact onClick={() => activateContact(4)} active={activeContact === 4} name='More' picture={image} state='state__active' message='Hola' notification='1'/>
+                <Contact onClick={() => activateContact(4)} active={activeContact === 4} name='More' picture={image} state='state__active' message='Hola' notification='1'/>
+                <Contact onClick={() => activateContact(4)} active={activeContact === 4} name='More' picture={image} state='state__active' message='Hola' notification='1'/>
+                <Contact onClick={() => activateContact(4)} active={activeContact === 4} name='More' picture={image} state='state__active' message='Hola' notification='1'/>
+            </div>
+  </div>
     )
 }

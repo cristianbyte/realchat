6fbff3a0cb6f99db487fabb7d03cdee67bf23429
cristianbyte <charlsmaritz@gmail.com>
2023-01-08 17:07:38 -0500

    finiggggg

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,16 +1,16 @@
 import { createContext, useEffect, useState } from 'react'
 import { onAuthStateChanged } from 'firebase/auth'
-import { auth } from '--/firebase'
+import { auth } from '../firebase'
 
 
 export const AuthContext = createContext()
 
 export const AuthContextProvider = ({children}) => {
-    const [currentUser, serCurrentUser] = useState({})
-    useEffect((=>{
+    const [currentUser, setCurrentUser] = useState({})
+    useEffect(()=>{
         onAuthStateChanged( auth, (user)=>{
             setCurrentUser(user)
         } )
-    }))
+    })
 }
 
